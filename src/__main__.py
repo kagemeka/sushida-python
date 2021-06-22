6edad9fefee7c25a3672d2db615375b05e47a65a
kagemeka <kagemeka1@gmail.com>
2021-06-22 16:28:49 +0900

    enhance: complete application (#8)

--- src/__main__.py
+++ src/__main__.py
@@ -93,6 +93,7 @@ def main():
   driver.get(
     url=cfg.scrape_url,
   )
+  time.sleep(1)
   elm = driver.find_element(
     by=By.CLASS_NAME,
     value='main_play',
@@ -133,9 +134,52 @@ def main():
   print(
     driver.get_window_rect()
   )
-  pyautogui.press('enter')
-  # elm.send_keys(Keys.SPACE)
-  time.sleep(10)
+  pyautogui.press('space')
+  time.sleep(3)
+  from io import BytesIO
+  from PIL import (
+    Image,
+    ImageOps,
+  )
+  import base64 
+
+  for i in range(1 << 6):
+    b64_img = (
+      elm.screenshot_as_base64
+    )
+    bytes_img = base64.b64decode(
+      b64_img,
+    )
+    img = Image.open(
+      BytesIO(bytes_img),
+    )
+    w, h = img.size
+    print(img)
+    print(w, h)
+    img = img.crop(
+      (80, 230, 420, 256)
+    )
+    img = ImageOps.grayscale(img)
+    img = img.convert('L')
+    img = img.point(
+      lut=lambda x: (
+        255 if x > 127 else 0
+      ),
+    )
+    img = ImageOps.invert(img)
+    img.save('a.png')
+    import pytesseract
+    txt = pytesseract.image_to_string(
+      img,
+    )
+
+    
+    print(txt.strip())
+    
+    pyautogui.write(txt.strip())
+    time.sleep(1)
+
+  time.sleep(120)
   
   driver.close()
 
